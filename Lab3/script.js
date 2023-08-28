1e20f2acf209abe5775fcc10e97bf75acb71085f
M1Pheus <139745094+M1Pheus@users.noreply.github.com>
2023-08-28 14:58:49 +0700

    Update

---
+++ Lab3/script.js
@@ -0,0 +1,18 @@
+var x=0;
+function onClick(){
+    console.log(x);
+    if(x<10){
+         x=x+1; 
+    }
+}
+
+function onClick1(){
+    console.log(x);
+    if(x>0){
+          x=x-1;
+    }
+}
+const button = document.querySelector('#Add');
+button.addEventListener('click',onClick);
+const button1 = document.querySelector('#minus');
+button1.addEventListener('click',onClick1);
